afe4b35da7576310512257659863b0537de29794
Karsten Koehler <karsten.koehler95@gmail.com>
2018-10-03 12:02:29 +0200

    initial commit

---
+++ src/main/java/de/karstenkoehler/bridges/BridgesTesterImpl.java
@@ -0,0 +1,15 @@
+package de.karstenkoehler.bridges;
+
+import de.feu.ps.bridges.shared.BridgesTester;
+
+public class BridgesTesterImpl implements BridgesTester {
+    @Override
+    public void testGeneratePuzzle(String filePath, int width, int height, int isles) {
+
+    }
+
+    @Override
+    public void testSolvePuzzle(String puzzlePath, String solutionPath) {
+
+    }
+}
---
+++ src/main/java/de/karstenkoehler/bridges/Main_Koehler_Karsten.java
@@ -0,0 +1,7 @@
+package de.karstenkoehler.bridges;
+
+public class Main_Koehler_Karsten {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
