889d2fca486b0e42d56310634a9f3b14ded55399
Karsten Koehler <karsten.koehler95@gmail.com>
2018-10-06 13:11:53 +0200

    ui logic for new puzzle dialog

--- src/main/java/de/karstenkoehler/bridges/ui/NewPuzzleController.java
+++ src/main/java/de/karstenkoehler/bridges/ui/NewPuzzleController.java
@@ -1,26 +1,67 @@
 package de.karstenkoehler.bridges.ui;
 
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class NewPuzzleController {
 
     @FXML
-    private RadioButton useRandom;
+    private RadioButton rbtnUseRandom;
     @FXML
-    private RadioButton chooseParameters;
+    private RadioButton rbtnChooseParameters;
     @FXML
-    private CheckBox chooseNumOfIslands;
+    private CheckBox cbxChooseNumOfIslands;
     @FXML
-    private TextField width;
+    private TextField txtWidth;
     @FXML
-    private TextField height;
+    private TextField txtHeight;
     @FXML
-    private TextField islands;
+    private TextField txtIslands;
+    @FXML
+    private Label lblWidth;
+    @FXML
+    private Label lblHeight;
 
+    @FXML
+    private void initialize() {
+        this.rbtnUseRandom.selectedProperty().addListener(useRandomSelected());
+        this.rbtnChooseParameters.selectedProperty().addListener(chooseParametersSelected());
+        this.cbxChooseNumOfIslands.selectedProperty().addListener(numOfIslandsSelected());
+    }
+
+    private ChangeListener<Boolean> useRandomSelected() {
+        return (observable, old, selected) -> {
+            if (selected) {
+                setElementsDisabled(true);
+            }
+        };
+    }
+
+    private ChangeListener<Boolean> chooseParametersSelected() {
+        return (observable, old, selected) -> {
+            if (selected) {
+                setElementsDisabled(false);
+            }
+        };
+    }
+
+    private ChangeListener<Boolean> numOfIslandsSelected() {
+        return ((observable, old, selected) -> txtIslands.setDisable(!selected));
+    }
+
+    private void setElementsDisabled(boolean value) {
+        cbxChooseNumOfIslands.setDisable(value);
+        txtWidth.setDisable(value);
+        txtHeight.setDisable(value);
+        txtIslands.setDisable(value);
+        lblWidth.setDisable(value);
+        lblHeight.setDisable(value);
+    }
 
     @FXML
     private void onOk(ActionEvent actionEvent) {
