71031a25545434f0dabbf82eb9d44e1f37d6fca0
SchiffFlieger <karsten.koehler95@gmail.com>
2018-10-17 18:17:43 +0200

    remove unnecessary code

--- src/main/java/de/karstenkoehler/bridges/Main_Koehler_Karsten.java
+++ src/main/java/de/karstenkoehler/bridges/Main_Koehler_Karsten.java
@@ -1,13 +1,5 @@
 package de.karstenkoehler.bridges;
 
-import de.karstenkoehler.bridges.model.BridgesPuzzle;
-import de.karstenkoehler.bridges.io.parser.ParseException;
-import de.karstenkoehler.bridges.io.parser.Parser;
-import de.karstenkoehler.bridges.io.parser.TokenConsumingParser;
-import de.karstenkoehler.bridges.io.parser.token.TokenizerImpl;
-import de.karstenkoehler.bridges.io.validator.DefaultValidator;
-import de.karstenkoehler.bridges.io.validator.ValidateException;
-import de.karstenkoehler.bridges.io.validator.Validator;
 import de.karstenkoehler.bridges.ui.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,20 +7,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Main_Koehler_Karsten extends Application {
     public static void main(String[] args) {
-//        mainConsole();
-        mainGui(args);
-        System.out.println("finish");
-    }
-
-    public static void mainGui(String[] args) {
         launch(args);
     }
 
@@ -49,32 +31,4 @@ public class Main_Koehler_Karsten extends Application {
         controller.setStage(stage);
         return root;
     }
-
-    public static void mainConsole() {
-        Validator validator = new DefaultValidator();
-
-        File dir = new File("src\\main\\resources\\data\\");
-        for (File file : dir.listFiles()) {
-            if (!file.isFile()) {
-                continue;
-            }
-
-            try {
-                Parser parser = new TokenConsumingParser(new TokenizerImpl(readFile(file.getAbsolutePath())));
-                BridgesPuzzle result = parser.parse();
-                validator.validate(result);
-                System.out.printf("%-30s %2dx%2d %3d islands, %3d bridges\n", file.getName(), result.getWidth(), result.getHeight(), result.getIslands().size(), result.getBridges().size());
-
-            } catch (IOException | ParseException | ValidateException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-
-    private static String readFile(String path) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(path)), Charset.defaultCharset());
-    }
-
-
 }
