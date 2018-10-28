253b44e90ac341718f422b1ecf606dc04fb0378d
Karsten Koehler <karsten.koehler95@gmail.com>
2018-10-28 09:11:18 +0100

    show state text

--- src/main/java/de/karstenkoehler/bridges/model/PuzzleState.java
+++ src/main/java/de/karstenkoehler/bridges/model/PuzzleState.java
@@ -1,5 +1,19 @@
 package de.karstenkoehler.bridges.model;
 
 public enum PuzzleState {
-    SOLVED, NOT_SOLVED, NO_LONGER_SOLVABLE, ERROR
+    SOLVED("Puzzle is solved."),
+    NOT_SOLVED("Puzzle is not solved."),
+    NO_LONGER_SOLVABLE("Puzzle is no longer solvable."),
+    ERROR("A rule has been violated.");
+
+    private final String state;
+
+    PuzzleState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return this.state;
+    }
 }
