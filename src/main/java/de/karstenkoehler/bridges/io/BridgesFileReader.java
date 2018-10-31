16548a88606b1e9cc7e831dcb7c524068a72e94d
Karsten Koehler <karsten.koehler95@gmail.com>
2018-10-31 19:51:15 +0100

    javadoc for io package

--- src/main/java/de/karstenkoehler/bridges/io/BridgesFileReader.java
+++ src/main/java/de/karstenkoehler/bridges/io/BridgesFileReader.java
@@ -15,24 +15,54 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+/**
+ * A bridges file reader can read puzzles from the hard drive. Therefore it uses instances of {@link TokenizerImpl}
+ * and {@link TokenConsumingParser}.
+ */
 public class BridgesFileReader {
+
+    /**
+     * Generates a {@link BridgesPuzzle} from the content of a {@link File}. The content of the file
+     * is parsed and validated. The returned puzzle is fully initialized and ready to use.
+     *
+     * @param file the file to read from
+     * @return the puzzle specified in the file
+     * @throws ParseException    if the file content had syntactic errors
+     * @throws ValidateException if the file content had semantic errors
+     * @throws IOException       if there where issues reading the file
+     */
     public BridgesPuzzle readFile(File file) throws ParseException, ValidateException, IOException {
-        BridgesPuzzle puzzle = parsePuzzle(file);
+        BridgesPuzzle puzzle = readAndParse(file);
         puzzle.fillMissingBridges();
         return puzzle;
     }
 
-    private BridgesPuzzle parsePuzzle(File file) throws IOException, ParseException, ValidateException {
+    /**
+     * Reads, parses and validates the puzzle specified in the file.
+     *
+     * @param file the file to read from
+     * @return the puzzle specified in the file
+     * @throws ParseException    if the file content had syntactic errors
+     * @throws ValidateException if the file content had semantic errors
+     * @throws IOException       if there where issues reading the file
+     */
+    private BridgesPuzzle readAndParse(File file) throws IOException, ParseException, ValidateException {
         Validator validator = new DefaultValidator();
 
         Parser parser = new TokenConsumingParser(new TokenizerImpl(readFileToString(file.getAbsolutePath())));
         BridgesPuzzle puzzle = parser.parse();
         validator.validate(puzzle);
 
-
         return puzzle;
     }
 
+    /**
+     * Reads the content of the given file into a string.
+     *
+     * @param path the file to read from
+     * @return the content of the file
+     * @throws IOException if there where issues reading the file
+     */
     private static String readFileToString(String path) throws IOException {
         return new String(Files.readAllBytes(Paths.get(path)), Charset.defaultCharset());
     }
--- src/main/java/de/karstenkoehler/bridges/io/BridgesFileWriter.java
+++ src/main/java/de/karstenkoehler/bridges/io/BridgesFileWriter.java
@@ -9,6 +9,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * A bridges file writer can write a puzzle to a file on the hard drive. For serializing the puzzles this class
+ * utilizes an instance of {@link SerializerImpl}.
+ */
 public class BridgesFileWriter {
     private final Serializer serializer;
 
@@ -16,6 +20,13 @@ public class BridgesFileWriter {
         serializer = new SerializerImpl();
     }
 
+    /**
+     * Serializes the given puzzle and writes the string to the specified file.
+     *
+     * @param file   the file to write to
+     * @param puzzle the puzzle to serialize
+     * @throws IOException if there where issues writing the file
+     */
     public void writeFile(File file, BridgesPuzzle puzzle) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.write(serializer.serialize(puzzle));
