f593381de3d1e9608b37ea905cd17e17bc5ef972
Karsten Koehler <karsten.koehler95@gmail.com>
2018-11-14 19:43:56 +0100

    improve solver

--- src/main/java/de/karstenkoehler/bridges/model/solver/SolverImpl.java
+++ src/main/java/de/karstenkoehler/bridges/model/solver/SolverImpl.java
@@ -5,6 +5,9 @@ import de.karstenkoehler.bridges.model.BridgesPuzzle;
 import de.karstenkoehler.bridges.model.Island;
 import de.karstenkoehler.bridges.model.Orientation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SolverImpl implements Solver {
     @Override
     public Bridge nextSafeBridge(BridgesPuzzle puzzle) {
@@ -19,25 +22,31 @@ public class SolverImpl implements Solver {
     }
 
     private Bridge findSaveBridge(Island island, BridgesPuzzle puzzle) {
-        int neighbors = getNeighborCount(island, puzzle);
+        int remaining = puzzle.getRemainingBridgeCount(island);
+        if (remaining == 0) {
+            return null;
+        }
 
-        if ((neighbors * 2) - island.getRequiredBridges() == 0) {
+        List<Bridge> neighbors = getPossibleNeighborBridges(island, puzzle);
+        int calc = (neighbors.size() * 2) - remaining;
+//        System.out.printf("neighbors: %d, remaining: %d, calc: %d%n", neighbors.size(), puzzle.getRemainingBridgeCount(island), calc);
+
+        if (calc == 0) {
             // every adjacent island has double bridge
-            Bridge bridge = assureAtLeastXBridges(island, puzzle, 2);
+            Bridge bridge = assureAtLeastXBridges(neighbors, 2);
             if (bridge != null) return bridge;
         }
-        if ((neighbors * 2) - island.getRequiredBridges() == 1) {
+        if (calc == 1) {
             // every adjacent island has at least single bridge
-            Bridge bridge = assureAtLeastXBridges(island, puzzle, 1);
+            Bridge bridge = assureAtLeastXBridges(neighbors, 2);
             if (bridge != null) return bridge;
         }
 
         return null;
     }
 
-    private Bridge assureAtLeastXBridges(Island island, BridgesPuzzle puzzle, int minBridgeCount) {
-        for (Orientation orientation : Orientation.values()) {
-            Bridge bridge = puzzle.getConnectedBridge(island, orientation);
+    private Bridge assureAtLeastXBridges(List<Bridge> bridges, int minBridgeCount) {
+        for (Bridge bridge : bridges) {
             if (bridge != null && bridge.getBridgeCount() < minBridgeCount) {
                 return bridge;
             }
@@ -45,14 +54,27 @@ public class SolverImpl implements Solver {
         return null;
     }
 
-    // TODO move to bridge class
-    private int getNeighborCount(Island island, BridgesPuzzle puzzle) {
-        int neighbors = 0;
+    private List<Bridge> getPossibleNeighborBridges(Island island, BridgesPuzzle puzzle) {
+        List<Bridge> result = new ArrayList<>(4);
+
         for (Orientation orientation : Orientation.values()) {
-            if (puzzle.getConnectedBridge(island, orientation) != null) {
-                neighbors++;
+            Bridge bridge = puzzle.getConnectedBridge(island, orientation);
+            if (bridge != null && bridge.getBridgeCount() < 2 && !puzzle.causesCrossing(bridge)) {
+                Island other = getOtherIsland(bridge, island);
+                if ((island.getRequiredBridges() == 1 && other.getRequiredBridges() == 1 && puzzle.getIslands().size() != 2) || puzzle.getRemainingBridgeCount(other) == 0) {
+                    continue;
+                }
+                result.add(bridge);
             }
         }
-        return neighbors;
+
+        return result;
+    }
+
+    private Island getOtherIsland(Bridge bridge, Island island) {
+        if (bridge.getStartIsland() == island) {
+            return bridge.getEndIsland();
+        }
+        return bridge.getStartIsland();
     }
 }
---
+++ src/test/java/de/karstenkoehler/bridges/test/solver/SolverEasyTest.java
@@ -0,0 +1,86 @@
+package de.karstenkoehler.bridges.test.solver;
+
+import de.karstenkoehler.bridges.model.Bridge;
+import de.karstenkoehler.bridges.model.BridgesPuzzle;
+import de.karstenkoehler.bridges.model.Island;
+import de.karstenkoehler.bridges.model.solver.Solver;
+import de.karstenkoehler.bridges.model.solver.SolverImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class SolverEasyTest {
+    private static Solver solver = new SolverImpl();
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        List<Island> bsp_5x5 = Arrays.asList(
+                new Island(0, 0, 0, 3),
+                new Island(1, 0, 2, 4),
+                new Island(2, 0, 4, 2),
+                new Island(3, 2, 0, 3),
+                new Island(4, 2, 3, 2),
+                new Island(5, 3, 2, 1),
+                new Island(6, 3, 4, 1),
+                new Island(7, 4, 0, 3),
+                new Island(8, 4, 3, 3)
+        );
+
+        List<Island> bsp_6x6 = Arrays.asList(
+                new Island(0, 0, 0, 1),
+                new Island(1, 0, 2, 4),
+                new Island(2, 0, 5, 3),
+                new Island(3, 2, 0, 4),
+                new Island(4, 2, 2, 7),
+                new Island(5, 2, 4, 3),
+                new Island(6, 3, 1, 2),
+                new Island(7, 3, 3, 2),
+                new Island(8, 3, 5, 3),
+                new Island(9, 4, 0, 2),
+                new Island(10, 4, 2, 1),
+                new Island(11, 4, 4, 1),
+                new Island(12, 5, 1, 3),
+                new Island(13, 5, 3, 5),
+                new Island(14, 5, 5, 3)
+        );
+        return Arrays.asList(new Object[][]{
+                {new BridgesPuzzle(bsp_5x5, new ArrayList<>(), 5, 5), 11},
+//                {new BridgesPuzzle(bsp_6x6, new ArrayList<>(), 6, 6), 20},
+        });
+    }
+
+    @Parameterized.Parameter
+    public BridgesPuzzle input;
+
+    @Parameterized.Parameter(1)
+    public int expectedBridges;
+
+    @Test
+    public void testFillBridges() {
+        this.input.fillMissingBridges();
+        int bridgesBuilt = 0;
+
+        Bridge next;
+        do {
+            next = solver.nextSafeBridge(this.input);
+
+            if (next != null) {
+                next.setBridgeCount(next.getBridgeCount() + 1);
+                bridgesBuilt++;
+            }
+        } while (next != null);
+
+        this.input.markInvalidBridges();
+        assertEquals(expectedBridges, bridgesBuilt);
+        assertTrue(input.getBridges().stream().allMatch(Bridge::isValid));
+    }
+}
