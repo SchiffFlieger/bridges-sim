f279c9c1bebf7ed35857bff50e1507e5e4b037e6
SchiffFlieger <karsten.koehler95@gmail.com>
2018-11-19 20:28:55 +0100

    test class implementation

--- src/main/java/de/karstenkoehler/bridges/BridgesTesterImpl.java
+++ src/main/java/de/karstenkoehler/bridges/BridgesTesterImpl.java
@@ -1,15 +1,78 @@
 package de.karstenkoehler.bridges;
 
 import de.feu.ps.bridges.shared.BridgesTester;
+import de.karstenkoehler.bridges.io.BridgesFileReader;
+import de.karstenkoehler.bridges.io.BridgesFileWriter;
+import de.karstenkoehler.bridges.io.parser.ParseException;
+import de.karstenkoehler.bridges.io.validator.DefaultValidator;
+import de.karstenkoehler.bridges.io.validator.ValidateException;
+import de.karstenkoehler.bridges.model.Bridge;
+import de.karstenkoehler.bridges.model.BridgesPuzzle;
+import de.karstenkoehler.bridges.model.PuzzleSpecification;
+import de.karstenkoehler.bridges.model.generator.Generator;
+import de.karstenkoehler.bridges.model.generator.GeneratorImpl;
+import de.karstenkoehler.bridges.model.solver.DefaultSolver;
+import de.karstenkoehler.bridges.model.solver.Solver;
+
+import java.io.File;
+import java.io.IOException;
 
 public class BridgesTesterImpl implements BridgesTester {
+    private Generator generator;
+    private Solver solver;
+    private BridgesFileWriter writer;
+    private BridgesFileReader reader;
+
+    public BridgesTesterImpl() {
+        this.generator = new GeneratorImpl(new DefaultValidator());
+        this.solver = new DefaultSolver();
+        this.writer = new BridgesFileWriter();
+        this.reader = new BridgesFileReader();
+    }
+
     @Override
     public void testGeneratePuzzle(String filePath, int width, int height, int isles) {
+        File file = new File(filePath);
+        if (file.exists()) {
+            throw new IllegalArgumentException("file already exists");
+        }
 
+        try {
+            PuzzleSpecification spec = PuzzleSpecification.withSpecs(false, width, height, isles);
+            writer.writeFile(file, generator.generate(spec));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void testSolvePuzzle(String puzzlePath, String solutionPath) {
+        File inFile = new File(puzzlePath);
+        if (!inFile.exists()) {
+            throw new IllegalArgumentException("file not found");
+        }
 
+        File outFile = new File(solutionPath);
+        if (outFile.exists()) {
+            throw new IllegalArgumentException("file already exists");
+        }
+
+        try {
+            BridgesPuzzle puzzle = reader.readFile(inFile);
+            puzzle.fillMissingBridges();
+
+            while (true) {
+                Bridge next = solver.nextSafeBridge(puzzle);
+                if (next == null) {
+                    break;
+                }
+                next.setBridgeCount(next.getBridgeCount() + 1);
+            }
+
+            writer.writeFile(outFile, puzzle);
+
+        } catch (ParseException | ValidateException | IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/de/karstenkoehler/bridges/test/BidgesTest_Koehler_Karsten.java
+++ src/test/java/de/karstenkoehler/bridges/test/BidgesTest_Koehler_Karsten.java
@@ -1,28 +1,28 @@
-//package de.karstenkoehler.bridges.test;
-//
-//import de.feu.ps.bridges.shared.BridgesTest;
-//import de.feu.ps.bridges.shared.BridgesTester;
-//import de.karstenkoehler.bridges.BridgesTesterImpl;
-//
-//public class BidgesTest_Koehler_Karsten extends BridgesTest {
-//
-//    @Override
-//    public BridgesTester getBridgesTesterImpl() {
-//        return new BridgesTesterImpl();
-//    }
-//
-//    @Override
-//    public String getMatrNr() {
-//        return "8690570";
-//    }
-//
-//    @Override
-//    public String getName() {
-//        return "Karsten Köhler";
-//    }
-//
-//    @Override
-//    public String getEmail() {
-//        return "karsten.koehler1@studium.fernuni-hagen.de";
-//    }
-//}
+package de.karstenkoehler.bridges.test;
+
+import de.feu.ps.bridges.shared.BridgesTest;
+import de.feu.ps.bridges.shared.BridgesTester;
+import de.karstenkoehler.bridges.BridgesTesterImpl;
+
+public class BidgesTest_Koehler_Karsten extends BridgesTest {
+
+    @Override
+    public BridgesTester getBridgesTesterImpl() {
+        return new BridgesTesterImpl();
+    }
+
+    @Override
+    public String getMatrNr() {
+        return "8690570";
+    }
+
+    @Override
+    public String getName() {
+        return "Karsten Köhler";
+    }
+
+    @Override
+    public String getEmail() {
+        return "karsten.koehler1@studium.fernuni-hagen.de";
+    }
+}
