0e99658e310a00cc14d6307358aa4ac0645582db
Karsten Koehler <karsten.koehler95@gmail.com>
2018-12-24 10:50:36 +0100

    javadoc for canvas dimensions

--- src/main/java/de/karstenkoehler/bridges/ui/CanvasDimensions.java
+++ src/main/java/de/karstenkoehler/bridges/ui/CanvasDimensions.java
@@ -1,5 +1,10 @@
 package de.karstenkoehler.bridges.ui;
 
+/**
+ * This class encapsulates some calculations for the size of the game objects on
+ * the canvas. Each puzzle is dimensioned accordingly, i.e. it uses the entire
+ * canvas area, regardless of its width and height.
+ */
 public class CanvasDimensions {
 
     private final int gridLines;
@@ -12,6 +17,12 @@ public class CanvasDimensions {
     private final double doubleBridgeOffset;
     private final double bridgeLineSize;
 
+    /**
+     * Creates a new dimensions objects.
+     *
+     * @param gridLines  the max of height and width of the puzzle
+     * @param canvasSize the max of height and width of the canvas
+     */
     public CanvasDimensions(final int gridLines, final double canvasSize) {
         this.gridLines = gridLines;
         this.fontSize = 500.0 / gridLines;
@@ -24,42 +35,93 @@ public class CanvasDimensions {
         this.fieldSize = (canvasSize - 2 * this.padding) / (this.gridLines - 1);
     }
 
+    /**
+     * Returns the number of grid lines in the puzzle.
+     *
+     * @return the number of grid lines in the puzzle
+     */
     public int getGridLines() {
         return gridLines;
     }
 
+    /**
+     * Returns the size of a single grid field.
+     *
+     * @return the size of a single grid field
+     */
     public double getFieldSize() {
         return fieldSize;
     }
 
+    /**
+     * Returns the size of the font for the island numbers.
+     *
+     * @return the size of the font for the island numbers
+     */
     public double getFontSize() {
         return fontSize;
     }
 
+    /**
+     * Returns the diameter of an island.
+     *
+     * @return the diameter of an island
+     */
     public double getIslandDiameter() {
         return islandDiameter;
     }
 
+    /**
+     * Returns the offset that is needed so the island is placed directly on the grid.
+     *
+     * @return the offset that is needed so the island is placed directly on the grid
+     */
     public double getIslandOffset() {
         return islandOffset;
     }
 
+    /**
+     * Returns the padding of the puzzle inside the canvas.
+     *
+     * @return the padding of the puzzle inside the canvas
+     */
     public double getPadding() {
         return padding;
     }
 
+    /**
+     * Returns the size of the click area.
+     *
+     * @return the size of the click area
+     */
     public double getClickAreaSize() {
         return clickAreaSize;
     }
 
+    /**
+     * Returns the offset that is needed to draw a double bridge.
+     *
+     * @return the offset that is needed to draw a double bridge
+     */
     public double getDoubleBridgeOffset() {
         return doubleBridgeOffset;
     }
 
+    /**
+     * Returns the thickness of a bridge.
+     *
+     * @return the thickness of a bridge
+     */
     public double getBridgeLineSize() {
         return bridgeLineSize;
     }
 
+    /**
+     * Transforms the puzzles x and y coordinates to the according x and y coordinate on the canvas.
+     *
+     * @param i the coordinate of the puzzle to transform
+     * @return the resulting coordinate on the canvas
+     */
     public double coordinate(int i) {
         return (i * (this.getFieldSize())) + this.getPadding();
     }
